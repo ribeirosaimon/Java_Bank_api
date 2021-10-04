5b72c8d9743e3dda0485d5385510d7696d2e49f8
Saimon Ribeiro <saimonribeiros@hotmail.com>
2021-10-04 09:20:55 -0300

    tests

--- src/test/java/br/com/saimon/JavaBankApplicationTests.java
+++ src/test/java/br/com/saimon/JavaBankApplicationTests.java
@@ -1,13 +1,29 @@
 package br.com.saimon;
 
+import br.com.saimon.converter.DozerConverter;
+import br.com.saimon.mocks.MockBank;
+import br.com.saimon.model.entities.BankClientVO;
+import org.junit.Assert;
+import org.junit.Before;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class JavaBankApplicationTests {
 
+	MockBank inputObject;
+
+	@Before
+	public void setUp(){
+		inputObject = new MockBank();
+	}
+
 	@Test
-	void contextLoads() {
+	void parseEntityToVOTest () {
+		BankClientVO output = DozerConverter.parseObject(inputObject.mockEntity(), BankClientVO.class);
+		Assert.assertEquals("teste", output.getName());
+
+
 	}
 
 }
